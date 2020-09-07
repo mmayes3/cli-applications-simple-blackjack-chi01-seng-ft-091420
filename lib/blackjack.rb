cc0eed0ace63dbf7f02f5a026516519aba701710
classy-tier-0049 <mauricelynell@gmail.com>
2020-09-07 20:39:33 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,69 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  random = rand(1..11)
+  return random
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
-end
-
-def hit?
-  # code hit? here
+  c1 = deal_card
+  c2 = deal_card
+  sum = c1 + c2
+  display_card_total(sum)
+  return sum
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
+def hit?(initial_round)
+  prompt_user
+  y = get_user_input
+  ir = initial_round
+  if y == 'h'
+    return current_card_total = deal_card + ir
+  elsif y == 's'
+    return ir
+  else
+    invalid_command
+    prompt_user
+    hit?(initial_round)
+  end
+end
+
 
 def runner
-  # code runner here
+  welcome
+  initial_round
+  prompt_user
+  y = prompt_user
+  if y == 's'
+    prompt_user
+  else
+    deal_card
+    new_total = deal_card + initial_round
+    display_card_total(new_total)
+    if new_total >= 21
+      end_game(new_total)
+    end
 end
     
